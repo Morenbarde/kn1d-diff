f32e30bd72c699041bd7df7a3eecf2d882041d6e
Nicholas Brown <njbrown@wm.edu>
2026-02-01 20:30:36 -0500

    added compensate_distribution translation and create_shifted_maxwellian plotting

--- kn1ddiff/create_shifted_maxwellian.py
+++ kn1ddiff/create_shifted_maxwellian.py
@@ -40,26 +40,43 @@ def compensate_distribution(f_slice, vdiff, vr, vx, vth, target_vx, target_energ
     
     #NOTE Get nb name checked
 
-    vth_diffs = np.zeros((vr.size, vx.size, 2), float)
-    vrvx_diffs = np.zeros((vr.size, vx.size, 2), float)
-    sign = [1,-1]
+    nvr = vr.numel()
+    nvx = vx.numel()
+
+
+    #Differential Values torch conversion, replace later with make_dvr_dvx conversion
+    dvr_vol = torch.from_numpy(vdiff.dvr_vol)
+    dvx = torch.from_numpy(vdiff.dvx)
+    vmag_squared = torch.from_numpy(vdiff.vmag_squared)
+    volume = torch.from_numpy(vdiff.volume)
+
+
+    # vth_diffs = np.zeros((vr.size, vx.size, 2), float)
+    vth_diffs = torch.zeros((nvr, nvx, 2))
+    # vrvx_diffs = np.zeros((vr.size, vx.size, 2), float)
+    vrvx_diffs = torch.zeros((nvr, nvx, 2))
 
     # Compute present moments of Maxwell, WxMax, and EMax (x_moment, energy_moment)
-    vx_moment = vth*np.sum(vdiff.dvr_vol*np.matmul(f_slice, (vx*vdiff.dvx))) / nb
-    energy_moment = (vth**2)*np.sum(vdiff.dvr_vol*np.matmul((vdiff.vmag_squared*f_slice), vdiff.dvx)) / nb
+    # vx_moment = vth*np.sum(dvr_vol*np.matmul(f_slice, (vx*dvx))) / nb
+    # energy_moment = (vth**2)*np.sum(dvr_vol*np.matmul((vmag_squared*f_slice), dvx)) / nb
+    vx_moment = vth*torch.sum(dvr_vol*(f_slice @ (vx*dvx))) / nb
+    energy_moment = (vth**2)*torch.sum(dvr_vol*((vmag_squared*f_slice) @ dvx)) / nb
 
     # Compute weighted function from distribution, padded with zeros
-    weighted_dist = np.zeros((vr.size+2, vx.size+2), dtype=np.float64) #NOTE Check with someone if this name is accurate
+    # weighted_dist = np.zeros((vr.size+2, vx.size+2), dtype=np.float64) #NOTE Check with someone if this name is accurate
+    weighted_dist = torch.zeros((nvr+2, nvx+2)) #NOTE Check with someone if this name is accurate
 
     #Shorthand slices for center of padded distribution
-    vr_center = slice(1, vr.size+1)
-    vx_center = slice(1, vx.size+1)
+    vr_center = slice(1, nvr+1)
+    vx_center = slice(1, nvx+1)
 
-    weighted_dist[vr_center, vx_center] = f_slice*vdiff.volume / nb
+    weighted_dist[vr_center, vx_center] = f_slice*volume / nb
 
     # Used for interp_fvrvxx
     # Run additional correction if the distribution is not assumed to be positive
     allow_neg = False
+    '''
+    # NOTE Not Autodifferentaible yet, revisit later
     if not assume_pos:
         cutoff = 1.0e-6*np.max(weighted_dist)
         ii = np.where((abs(weighted_dist) < cutoff) & (abs(weighted_dist) > 0))
@@ -68,18 +85,23 @@ def compensate_distribution(f_slice, vdiff, vr, vx, vth, target_vx, target_energ
 
         if max(weighted_dist[2,:]) <= 0:
             allow_neg = True
+    '''
 
-    vx_dist = weighted_dist*vdiff.vx_dvx
-    vr_dist = weighted_dist*vdiff.vr_dvr
-    vth_dist = weighted_dist*vdiff.vth_dvx
+    vx_dist = weighted_dist*torch.from_numpy(vdiff.vx_dvx)
+    vr_dist = weighted_dist*torch.from_numpy(vdiff.vr_dvr)
+    vth_dist = weighted_dist*torch.from_numpy(vdiff.vth_dvx)
 
     # Compute Ap, Am, Bp, and Bm (0=p 1=m)
 
-    diff_padded = np.roll(vth_dist, shift=1, axis=1) - vth_dist
-    vth_diffs[:,:,0]   = np.copy(diff_padded[vr_center, vx_center])
+    # diff_padded = np.roll(vth_dist, shift=1, axis=1) - vth_dist
+    # vth_diffs[:,:,0]   = np.copy(diff_padded[vr_center, vx_center])
+    diff_padded = torch.roll(vth_dist, shifts=1, dims=1) - vth_dist
+    vth_diffs[:,:,0]   = torch.clone(diff_padded[vr_center, vx_center])
     
-    diff_padded = -np.roll(vth_dist, shift=-1, axis=1) + vth_dist
-    vth_diffs[:,:,1]   = np.copy(diff_padded[vr_center, vx_center])
+    # diff_padded = -np.roll(vth_dist, shift=-1, axis=1) + vth_dist
+    # vth_diffs[:,:,1]   = np.copy(diff_padded[vr_center, vx_center])
+    diff_padded = -torch.roll(vth_dist, shifts=-1, dims=1) + vth_dist
+    vth_diffs[:,:,1]   = torch.clone(diff_padded[vr_center, vx_center])
 
     # Indices for pos/neg vx values
     pos_start = vdiff.vx_pos_start  # First positive vx index
@@ -87,18 +109,21 @@ def compensate_distribution(f_slice, vdiff, vr, vx, vth, target_vx, target_energ
     neg_start = vdiff.vx_neg_start  # First negative vx index
     neg_end   = vdiff.vx_neg_end    # Last negative vx index
 
-    vrvx_diffs[:, pos_start+1:pos_end+1, 0] =  vx_dist[vr_center, pos_start+1:pos_end+1] - vx_dist[vr_center, pos_start+2:pos_end+2]
+    vrvx_diffs[:, pos_start+1:pos_end+1, 0] =  vx_dist[vr_center, pos_start+1:pos_end+1]             - vx_dist[vr_center, pos_start+2:pos_end+2]
     vrvx_diffs[:, pos_start, 0]             = -vx_dist[vr_center, pos_start+1]
     vrvx_diffs[:, neg_end, 0]               =  vx_dist[vr_center, neg_end+1]
-    vrvx_diffs[:, neg_start:neg_end, 0]     = -vx_dist[vr_center, neg_start+2:neg_end+2] + vx_dist[vr_center, neg_start+1:neg_end+1]
-    vrvx_diffs[:,:,0]                      +=  vr_dist[0:vr.size, vx_center]             - vr_dist[vr_center, vx_center     ]
+    vrvx_diffs[:, neg_start:neg_end, 0]     = -vx_dist[vr_center, neg_start+2:neg_end+2]             + vx_dist[vr_center, neg_start+1:neg_end+1]
+    # vrvx_diffs[:,:,0]                      +=  vr_dist[0:vr.size, vx_center]             - vr_dist[vr_center, vx_center     ]
+    vrvx_diffs[:,:,0]                       =  vrvx_diffs[:,:,0] + vr_dist[0:nvr, vx_center]         - vr_dist[vr_center, vx_center     ]
 
-    vrvx_diffs[:, pos_start+1:pos_end+1, 1] = -vx_dist[vr_center, pos_start+3:pos_end+3] + vx_dist[vr_center, pos_start+2:pos_end+2]
+    vrvx_diffs[:, pos_start+1:pos_end+1, 1] = -vx_dist[vr_center, pos_start+3:pos_end+3]             + vx_dist[vr_center, pos_start+2:pos_end+2]
     vrvx_diffs[:, pos_start, 1]             = -vx_dist[vr_center, pos_start+2]
     vrvx_diffs[:, neg_end, 1]               =  vx_dist[vr_center, neg_end]
-    vrvx_diffs[:, neg_start:neg_end, 1]     =  vx_dist[vr_center, neg_start:neg_end]     - vx_dist[vr_center, neg_start+1:neg_end+1]
-    vrvx_diffs[1:vr.size, :, 1]            +=  vr_dist[2:vr.size+1, vx_center]           - vr_dist[3:vr.size+2, vx_center]
-    vrvx_diffs[0,:,1]                      -=  vr_dist[2, vx_center]
+    vrvx_diffs[:, neg_start:neg_end, 1]     =  vx_dist[vr_center, neg_start:neg_end]                 - vx_dist[vr_center, neg_start+1:neg_end+1]
+    # vrvx_diffs[1:vr.size, :, 1]            +=  vr_dist[2:vr.size+1, vx_center]           - vr_dist[3:vr.size+2, vx_center]
+    vrvx_diffs[1:nvr, :, 1]                 =  vrvx_diffs[1:nvr, :, 1] + vr_dist[2:nvr+1, vx_center] - vr_dist[3:nvr+2, vx_center]
+    # vrvx_diffs[0,:,1]                      -=  vr_dist[2, vx_center]
+    vrvx_diffs[0,:,1]                       =  vrvx_diffs[0,:,1] - vr_dist[2, vx_center]
 
     #   If negative values for weighted distribution must be allowed, then add postive particles to i=0 and negative particles to i=1 (beta is negative here)
     if allow_neg:
@@ -106,26 +131,33 @@ def compensate_distribution(f_slice, vdiff, vr, vx, vth, target_vx, target_energ
         vrvx_diffs[1,:,1] = vrvx_diffs[1,:,1] + vr_dist[1,vx_center]
 
     # Remove padded zeros in weighted distribution
-    weighted_dist = weighted_dist[vr_center,vx_center]
+    weighted_dist = weighted_dist[vr_center, vx_center]
 
     # Cycle through 4 possibilies of sign(a_Max),sign(b_Max)
-    TB1 = np.zeros(2, float)
-    TB2 = np.zeros(2, float)
+    # TB1 = np.zeros(2, float)
+    # TB2 = np.zeros(2, float)
+    TB1 = torch.zeros(2)
+    TB2 = torch.zeros(2)
+    sign = [1,-1]
     
     for ia in range(2):
 
         # Compute TA1, TA2
-        TA1 = vth*np.sum(np.matmul(vth_diffs[:,:,ia], vx))
-        TA2 = (vth**2)*np.sum(vdiff.vmag_squared*vth_diffs[:,:,ia])
+        # TA1 = vth*np.sum(np.matmul(vth_diffs[:,:,ia], vx))
+        # TA2 = (vth**2)*np.sum(vmag_squared*vth_diffs[:,:,ia])
+        TA1 = vth*torch.sum(vth_diffs[:,:,ia] @ vx)
+        TA2 = (vth**2)*torch.sum(vmag_squared*vth_diffs[:,:,ia])
 
         for ib in range(2):
 
             # Compute TB1, TB2
             if TB1[ib] == 0:
-                TB1[ib] = vth*np.sum(np.matmul(vrvx_diffs[:,:,ib], vx))
+                # TB1[ib] = vth*np.sum(np.matmul(vrvx_diffs[:,:,ib], vx))
+                TB1[ib] = vth*torch.sum(vrvx_diffs[:,:,ib] @ vx)
 
             if TB2[ib] == 0:
-                TB2[ib] = (vth**2)*np.sum(vdiff.vmag_squared*vrvx_diffs[:,:,ib])
+                # TB2[ib] = (vth**2)*np.sum(vmag_squared*vrvx_diffs[:,:,ib])
+                TB2[ib] = (vth**2)*torch.sum(vmag_squared*vrvx_diffs[:,:,ib])
 
             denom = TA2*TB1[ib] - TA1*TB2[ib]
 
@@ -135,23 +167,26 @@ def compensate_distribution(f_slice, vdiff, vr, vx, vth, target_vx, target_energ
                 vrvx_scalar = (TA2*(target_vx - vx_moment) - TA1*(target_energy - energy_moment)) / denom
                 vth_scalar = (target_vx - vx_moment - TB1[ib]*vrvx_scalar) / TA1
                 
-            do_break = (vth_scalar*sign[ia] > 0) and (vrvx_scalar*sign[ib] > 0)
+            # do_break = (vth_scalar*sign[ia] > 0) and (vrvx_scalar*sign[ib] > 0)
                 #End While Loops
-            if do_break:
-                break
-        if do_break:
-            break
+        #     if do_break:
+        #         break
+        # if do_break:
+        #     break
 
     correction = vth_diffs[:,:,ia]*vth_scalar + vrvx_diffs[:,:,ib]*vrvx_scalar
 
     # Additional stuff for interp_fvrvxx
     s = 1
+    '''
+    # NOTE Not Autodifferentaible yet, revisit later
     if (not assume_pos) and (not allow_neg):
         ii = np.nonzero(weighted_dist)
         if np.size(ii) > 0:
             s = min(1/np.max(-correction[ii]/weighted_dist[ii]),1)
+    '''
 
-    f_slice = nb*(weighted_dist + s*correction) / vdiff.volume
+    f_slice = nb*(weighted_dist + s*correction) / volume
 
     return f_slice, s
 
@@ -234,18 +269,17 @@ def create_shifted_maxwellian(vr, vx, Tmaxwell, vx_shift, mu, mol, Tnorm):
         arg = torch.clamp(arg, min=-80.0, max=0.0)
         f = torch.exp(arg)
 
-        variable = f @ dvx
-
-        f = f / torch.nansum(dvr_vol*variable)
+        f = f / torch.nansum(dvr_vol*(f @ dvx))
 
         # Target energy density
         target_energy = (vx_shift[k]**2) + (3*CONST.Q*Tmaxwell[k] / (mol*mu*CONST.H_MASS))
 
         with torch.no_grad():
-            compensated_f_np, _ = compensate_distribution(f.cpu().numpy(), vdiff, vr.cpu().numpy(), vx.cpu().numpy(), vth.cpu().numpy(), vx_shift.cpu().numpy()[k], target_energy.cpu().numpy())
-            compensated_f = torch.from_numpy(compensated_f_np).to(f.device)
+            compensated_f, _ = compensate_distribution(f.detach(), vdiff, vr, vx, vth, vx_shift[k], target_energy)
 
         f = f + (compensated_f - f).detach()
+
+        # f, _ = compensate_distribution(f, vdiff, vr, vx, vth, vx_shift[k], target_energy)
 
         maxwell[:,:,k] = f / torch.sum(dvr_vol*(f @ dvx))
 
--- kn1ddiff/test/shift_maxwell/optimize.py
+++ kn1ddiff/test/shift_maxwell/optimize.py
@@ -1,16 +1,36 @@
 import torch
 import numpy as np
+import matplotlib.pyplot as plt
 
 from kn1ddiff.create_shifted_maxwellian import *
 
 
+def generate_compare_plot(dir, title, x, y, true_x, true_y, xlabel="", ylabel=""):
+    plt.plot(x, y, color = 'blue', marker='x', markersize=3, markeredgecolor='cyan', label="Optimized")
+    plt.plot(true_x, true_y, color = 'orange', marker='x', markersize=3, markeredgecolor='red', label="True", ls=":")
+    plt.title(title)
+    plt.xlabel(xlabel)
+    plt.ylabel(ylabel)
+    plt.legend()
+    plt.savefig(dir + title + '.png', dpi=300)
+    plt.clf()
 
 
+
+dir = "kn1ddiff/test/shift_maxwell/"
 data_file = "in_out2.npz"
 num_iters = 500
 
 
 if __name__ == "__main__":
+
+    use_cuda = torch.cuda.is_available()
+    device = torch.device("cuda" if use_cuda else "cpu")
+    print("device: ", device)
+    # if use_cuda:
+    #     torch.cuda.manual_seed(72)
+
+    torch.set_default_dtype(torch.float64)
     
     data = np.load("kn1ddiff/test/shift_maxwell/"+data_file)
     print(data["mol"])
@@ -32,23 +52,24 @@ if __name__ == "__main__":
 
     # Desired Outputs
     maxwell_old = torch.from_numpy(data["maxwell"])
-    # with torch.no_grad():
-    #     maxwell_old = create_shifted_maxwellian(vr, vx, Tmaxwell, vx_shift, mu, mol, Tnorm)
+    with torch.no_grad():
+        maxwell_old2 = create_shifted_maxwellian(vr, vx, Tmaxwell, vx_shift, mu, mol, Tnorm)
+
+    # print(torch.nn.functional.mse_loss(maxwell_old, maxwell_old2).item())
+    # input()
 
 
     # --- Test Optimization ---
     initial_tmaxwell = torch.nn.Parameter(torch.randn_like(Tmaxwell))
     initial_vx_shift = torch.nn.Parameter(torch.randn_like(vx_shift))
 
-    optimizer = torch.optim.Adam([initial_tmaxwell, initial_vx_shift], lr=1e-1)
+    optimizer = torch.optim.Adam([initial_tmaxwell, initial_vx_shift], lr=1.5e-1)
     scheduler = torch.optim.lr_scheduler.ReduceLROnPlateau(
         optimizer,
         factor=0.1,
         patience=30,
-        min_lr=1e-8
+        min_lr=1e-6
     )
-
-    loss_fun = torch.nn.MSELoss()
 
     for epoch in range(num_iters):
         tmax = 600 * torch.sigmoid(initial_tmaxwell)
@@ -59,7 +80,7 @@ if __name__ == "__main__":
         maxwell = create_shifted_maxwellian(vr, vx, tmax, shift, mu, mol, Tnorm)
 
         # Compute Loss
-        loss = loss_fun(maxwell, maxwell_old)
+        loss = torch.nn.functional.mse_loss(maxwell, maxwell_old)
 
 
         # Backprop
@@ -78,24 +99,62 @@ if __name__ == "__main__":
             f"loss: {loss.item():<10.6e} | "
             f"learning rate: {optimizer.param_groups[0]['lr']:.2e}"
         )
-        # print(initial_tmaxwell.grad)
-        # print(initial_vx_shift.grad)
-        # input()
 
-    print(torch.min(Tmaxwell), torch.max(Tmaxwell))
-    print(torch.min(vx_shift), torch.max(vx_shift))
-    input()
 
-    print(Tmaxwell)
-    print(tmax)
+    # --- Convert to numpy for analysis ---
+
+    tmax_loss = torch.nn.functional.mse_loss(tmax, Tmaxwell).item()
+    vxshift_loss = torch.nn.functional.mse_loss(shift, vx_shift).item()
+    max_loss = torch.nn.functional.mse_loss(maxwell, maxwell_old).item()
+
+    true_tmax = Tmaxwell.cpu().detach().numpy()
+    opt_tmax = tmax.cpu().detach().numpy()
+
+    true_vxshift = vx_shift.cpu().detach().numpy()
+    opt_vxshift = shift.cpu().detach().numpy()
+
+    true_maxwell = maxwell_old.cpu().detach().numpy()
+    opt_maxwell = maxwell.cpu().detach().numpy()
+
+
+    # --- Analyze ---
+
+    print("True Tmaxwell Range")
+    print(np.min(true_tmax), np.max(true_tmax))
+    print("Calculated Tmaxwell Range")
+    print(np.min(opt_tmax), np.max(opt_tmax))
     print()
-    print(vx_shift)
-    print(shift)
+    print("True vx_shift range")
+    print(np.min(true_vxshift), np.max(true_vxshift))
+    print("Calculated vx_shift range")
+    print(np.min(opt_vxshift), np.max(opt_vxshift))
     input()
 
-    print(maxwell_old[0])
-    print(maxwell[0])
+    print("True Tmaxwell")
+    print(true_tmax)
+    print("Calculated Tmaxwell")
+    print(opt_tmax)
+    print("Tmaxwell Loss: ", tmax_loss)
+    print()
+    print("True vx_shift")
+    print(true_vxshift)
+    print("Calculated vx_shift")
+    print(opt_vxshift)
+    print("vx_shift Loss: ", vxshift_loss)
 
-    print(torch.sum(maxwell_old[0,:,:]))
-    print(torch.sum(maxwell[0,:,:]))
+    x = range(opt_tmax.size)
+    generate_compare_plot(dir, "Tmaxwell", x, opt_tmax, x, true_tmax)
+    x = range(opt_vxshift.size)
+    generate_compare_plot(dir, "vx_shift", x, opt_vxshift, x, true_vxshift)
+
+    input()
+
+    # print(maxwell_old[0])
+    # print(maxwell[0])
+
+    print("True Maxwell Sum:       ", np.sum(true_maxwell))
+    print("Calculated Maxwell Sum: ", np.sum(opt_maxwell))
+    print("Maxwell Loss: ", max_loss)
+
+
 
